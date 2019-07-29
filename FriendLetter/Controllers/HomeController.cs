a7a8fcf45f8ccd4e031c55da9cd43f42a7a345e4
Student <example@example.com>
2019-07-29 11:29:03 -0700

    Add star wars mad libs to the project for reference later.

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using FriendLetter.Models;
 
+
 namespace FriendLetter.Controllers
 {
     public class HomeController : Controller
@@ -22,6 +23,7 @@ namespace FriendLetter.Controllers
             return View(myLetterVariable);
         }
 
+
         [Route("/form")]
         public ActionResult Form() { return View(); }
 
@@ -34,6 +36,33 @@ namespace FriendLetter.Controllers
             myLetterVariable.Location = location;
             myLetterVariable.Souvenir = souvenir;
             return View(myLetterVariable);
+        }
+
+        [Route("/madlibsform")]
+        public ActionResult Madlibsform() { return View(); }
+
+        [Route("/madlibs")]
+        public ActionResult Madlibs(string propNoun1, string propNoun2, string place1, string place2, string person1, string person2, string noun1, string noun2, string noun3, string verb1, string verb2, string verb3, string adj1, string adj2, string adj3)
+        {
+            MadLibVariable myMadLibVariable = new MadLibVariable();
+            myMadLibVariable.PropNoun1 = propNoun1;
+            myMadLibVariable.PropNoun2 = propNoun2;
+            myMadLibVariable.Place1 = place1;
+            myMadLibVariable.Place2 = place2;
+            myMadLibVariable.Person1 = person1;
+            myMadLibVariable.Person2 = person2;
+            myMadLibVariable.Noun1 = noun1;
+            myMadLibVariable.Noun2 = noun2;
+            myMadLibVariable.Noun3 = noun3;
+            myMadLibVariable.Verb1 = verb1;
+            myMadLibVariable.Verb2 = verb2;
+            myMadLibVariable.Verb3 = verb3;
+            myMadLibVariable.Adj1 = adj1;
+            myMadLibVariable.Adj2 = adj2;
+            myMadLibVariable.Adj3 = adj3;
+
+
+            return View(myMadLibVariable);
         }
     }
 }
--- FriendLetter/Models/LetterVariable.cs
+++ FriendLetter/Models/LetterVariable.cs
@@ -7,4 +7,26 @@ namespace FriendLetter.Models
         public string Souvenir { get; set; }
         public string Location { get; set; }
     }
+
+    public class MadLibVariable
+    {
+
+        public string PropNoun1 { get; set; }
+        public string PropNoun2 { get; set; }
+        public string Place1 { get; set; }
+        public string Place2 { get; set; }
+        public string Person1 { get; set; }
+        public string Person2 { get; set; }
+        public string Noun1 { get; set; }
+        public string Noun2 { get; set; }
+
+        public string Noun3 { get; set; }
+        public string Verb1 { get; set; }
+        public string Verb2 { get; set; }
+        public string Verb3 { get; set; }
+        public string Adj1 { get; set; }
+        public string Adj2 { get; set; }
+        public string Adj3 { get; set; }
+
+    }
 }
